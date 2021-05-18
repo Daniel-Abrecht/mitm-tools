1682600055f24cbd2ce1078b76f30a863af354ed
Daniel Abrecht <public@danielabrecht.ch>
2021-05-18 23:33:19 +0200

    Create m3u:* files when browser downloads EXTM3U files, and put corresponding local file names in it

--- save_http_files.sh
+++ save_http_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OLDIFS="$IFS"
+
 set -e
 
 exec 99<&0 0</dev/null
@@ -11,7 +13,9 @@ quote(){
   printf "'%s'" "$quoted"
 }
 
-dest=":$(sed "s|/|∕|g" <<<"$1$2")"
+domain="$1"; shift
+location="$1"; shift
+dest=":$(sed "s|/|∕|g" <<<"$domain$location")"
 
 exec 9>".lock$dest"
 flock 9 || exit 1
@@ -100,4 +104,89 @@ then
 else
   echo "Saving content at: $(quote "f$dest")"
   dd bs=4096 of="f$dest" conv=notrunc <&99
+fi
+
+url2local(){
+  url="$1"
+  printf 'f:'
+  if grep -q '^[a-zA-Z0-9+-]*://' <<<"$url"
+    then sed 's|^[a-zA-Z0-9+-]*://||;s|/|∕|g' <<<"$url"
+  elif [ "${url[0]}" = / ]
+    then sed 's|/|∕|g' <<<"$domain$url"
+    else sed 's|/|∕|g' <<<"$domain$(dirname "$(grep -o '^[^?]*' <<<"$location")XXX" | grep -v '^\.$')/$url"
+  fi
+}
+
+recombine_extm3u(){
+
+  sequence="$(grep '#EXT-X-MEDIA-SEQUENCE:' <"f$dest" | head -n 1 | grep -o '[0-9]*' | head -c 1)"
+  IFS=$'\n'
+  newlines=( $(grep -v '^[#]\|^$' <"f$dest") )
+  IFS="$OLDIFS"
+  oldlines=()
+
+  if [ -f "m3u$dest" ]
+  then
+    IFS=$'\n'
+    oldsequence="$(grep '#EXT-X-MEDIA-SEQUENCE:' <"m3u$dest" | head -n 1 | grep -o '[0-9]*' | head -c 1)"
+    IFS="$OLDIFS"
+    if [ -n "$oldsequence" ] && [ -n "$sequence" ]
+      then oldlines=( $(grep -v '^[#]\|^$' <"m3u$dest") )
+      else oldsequence="$sequence"
+    fi
+  fi
+
+  if [ -z "$oldsequence" ] || [ -z "$sequence" ]
+  then
+    (
+      echo "#EXTM3U"
+      if [ -n "$sequence" ]
+        then echo "#EXT-X-MEDIA-SEQUENCE: $sequence"
+      fi
+      for entry in "${newlines[@]}"
+        do url2local "$entry"
+      done
+    ) >"m3u$dest"
+  fi
+
+  sequence_min="$sequence"
+  if [ "$sequence_min" -le "$oldsequence" ]
+    then sequence_min="$oldsequence"
+  fi
+
+  nseq_end="$(bc <<<"$sequence + ${#newlines[@]}")"
+  oseq_end="$(bc <<<"$oldsequence + ${#oldlines[@]}")"
+  sequence_max="$nseq_end"
+  if [ "$sequence_max" -le "$oseq_end" ]
+    then sequence_max="$oseq_end"
+  fi
+
+  # Sequences probably unrelated
+  if [ "$(bc <<<"$sequence_max - $sequence_min")" -gt 100000 ]
+    then return # TODO: just copy new file
+  fi
+
+  (
+    echo "#EXTM3U"
+    echo "#EXT-X-MEDIA-SEQUENCE: $sequence_min"
+    i="$sequence_min"
+    while [ "$i" -le "$sequence_max" ]
+    do
+      if [ "$i" -ge "$sequence" ] && [ "$i" -lt "$nseq_end" ]
+        then url2local "${newlines[$i]}"
+      elif [ "$i" -ge "$oldsequence" ] && [ "$i" -lt "$oseq_end" ]
+        then url2local "${oldlines[$i]}"
+        else echo $?
+      fi
+      i="$(expr "$i" + 1)"
+    done
+  ) >"m3u$dest"
+}
+
+if [ -f "f$dest" ]
+then
+  read -n 20 header <"f$dest"
+  if [ "$header" = "#EXTM3U" ]
+    then recombine_extm3u
+  fi
 fi
